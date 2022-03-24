46a7086f0def1920c5c156fe061942decbf7fbc0
Jacob Valenzuela <valen306@cougars.csusm.edu>
2022-03-23 21:05:06 -0700

    Implemented Delete user.

--- src/main/java/com/jacobo/CRUDApp/UserRepository.java
+++ src/main/java/com/jacobo/CRUDApp/UserRepository.java
@@ -3,5 +3,5 @@ package com.jacobo.CRUDApp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    public Long countById(Integer id);
 }
